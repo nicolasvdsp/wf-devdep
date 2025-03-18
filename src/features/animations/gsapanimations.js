df39a01751ed5fda90d6a9f07da02d25fe6bab5d
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2025-03-18 14:30:22 +0100

    feat(gsap): adds gsap

--- src/features/animations/gsapanimations.js
+++ src/features/animations/gsapanimations.js
@@ -6,7 +6,20 @@ import { CustomEase } from 'gsap/CustomEase';
 gsap.registerPlugin(ScrollTrigger, SplitText, CustomEase);
 
 function allGsapAnimations() {
-
+  gsap.to(".animateme",
+    {
+      duration: 2,
+      rotateZ: 360,
+      ease: "power2.inOut",
+      scrollTrigger: {
+        trigger: ".animateme",
+        start: "top 80%",
+        end: "bottom 20%",
+        scrub: true,
+        markers: true
+      }
+    }
+  )
 }
 
 export default allGsapAnimations;
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import './scss/app.scss';
 import customAnimations from './features/animations/animations';
 import changesOnBreakpoints from './features/breakpoints';
 
+console.log("production is on!");
+
 customAnimations();
 changesOnBreakpoints();
